361872f0691245449e55cc554c57b75c1ff422ad
Serhii <grammas56@gmail.com>
2022-10-06 15:38:54 +0300

    JS_90.3

--- js/script2.js
+++ js/script2.js
@@ -1,40 +1,39 @@
-'use strict';
-
-class Menucard {
-    constructor(img,title,price){
+class MenuCard{
+    constructor(img,altimg,title,descr,price){
         this.img=img;
+        this.altimg=altimg;
         this.title=title;
+        this.descr=descr;
         this.price=price;
-    }
+    };
     render(){
         const elem = document.createElement('div');
-        elem.classList.add('menu__item');
-        elem.innerHTML=`
-                <img src=${this.img}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr"></div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
-        `;
         const par = document.querySelector('.menu__item_parent');
+        elem.classList.add('menu__item');
         par.append(elem);
+        elem.innerHTML = `
+        <img src= ${this.img} alt=${this.altimg}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+        `;
     }
 }
 
-
 fetch('db.json')
-.then(response => response.json())
+.then(response=>response.json())
 .then(data => data.menu)
-.then(menu => 
-    menu.forEach(obj=>
-        new Menucard (
-            obj.img,
-            obj.title,
-            obj.price
-        ).render()
-        ));
-
+.then(data => data.forEach(menu =>{
+    new MenuCard (
+        menu.img,
+        menu.altimg,
+        menu.title,
+        menu.descr,
+        menu.price
+    ).render();
+}));
 
